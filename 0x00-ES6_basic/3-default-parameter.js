20d0af1a55ec040d5ef1a61c616e6b75d1ee752c
tabby400 <tabbymargaret@gmail.com>
2024-01-03 14:08:19 +0000

    sum of init numbers

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,8 @@
+// Calculates the sum of an initial number and two expansion values. Returns the result.
+export default function getSumOfHoods(
+  initialNumber,
+  expansion1989 = 89,
+  expansion2019 = 19,
+) {
+  return initialNumber + expansion1989 + expansion2019;
+}
