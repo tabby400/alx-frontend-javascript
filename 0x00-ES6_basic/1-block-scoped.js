458061312ce6a52f318f16a78f4c9f08e430e9f4
tabby400 <tabbymargaret@gmail.com>
2024-01-03 13:47:46 +0000

    no empty rule

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  // eslint-disable-next-line no-empty
   // empty maybe for future use
+  // eslint-disable-next-line no-empty
   if (trueOrFalse) {
   }
 
