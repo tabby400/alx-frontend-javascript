99936877501f10636e3b536b374a2c0f3f1eb940
tabby400 <tabbymargaret@gmail.com>
2024-01-03 14:48:18 +0000

    for of

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+// adds a specified str to each value in the given array
+export default function appendToEachArrayValue(array, appendString) {
+  const res = [];
+  for (const value of array) {
+    res.push(appendString + array[array.indexOf(value)]);
+  }
+
+  return res;
+}
