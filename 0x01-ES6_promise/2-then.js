fe682933f02b1e815b66ce7af430eafbd048d601
tabby400 <tabbymargaret@gmail.com>
2024-01-04 08:43:16 +0000

    handling response

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,4 @@
+// handles the response from a promise
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => { console.log('Got a response from the API'); });
+}
