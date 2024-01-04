f72a1e58c8573ea79a94badf6f74500be9961d95
tabby400 <tabbymargaret@gmail.com>
2024-01-04 09:39:18 +0000

    value of math appended to queue

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+//captures the result or an error, and adds a processing message to the queue
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const res = mathFunction();
+    queue.push(res);
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
