72f8ce1bcc273dafa8be41f6484dee71376b2a9d
tabby400 <tabbymargaret@gmail.com>
2024-01-05 08:41:31 +0000

    dollars

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+// currency with properties such as code and name
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string') throw new Error();
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
