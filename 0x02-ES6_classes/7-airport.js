e247b950a3da8077fb2c3b6815424b38b8b59c9f
tabby400 <tabbymargaret@gmail.com>
2024-01-05 09:22:59 +0000

    task 7 8 9

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,28 @@
+// a class named airport with properties name and code
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' || typeof code !== 'string') throw new Error();
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this.code;
+  }
+}
